ac6cc508174606ae5bd148808550941b2964d982
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2019-12-04 16:29:14 +0100

    Show/hide sidebar

---
+++ Web/MintPlayer.Web/ClientApp/src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  declarations: [
+    SidebarComponent
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class ComponentsModule { }
